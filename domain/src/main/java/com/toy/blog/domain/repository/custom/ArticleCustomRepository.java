f921a878491a76e751dedc00f7b5dcb8a212f1a3
clapbin <clapbin0320@gmail.com>
2023-04-23 00:52:13 +0900

    feat: ArticleCustomRepository 생성
    - ArticleRepositoryCustom 삭제
    - ArticleCustomRepository 생성
    - 게시글 목록 조회, id로 게시글 조회, 조회수 증가, 게시글 수정, 게시글 삭제 메소드 생성
    - 팔로우한 친구 게시글 목록 메소드 주석 처리

--- domain/src/main/java/com/toy/blog/domain/repository/custom/ArticleCustomRepository.java
+++ domain/src/main/java/com/toy/blog/domain/repository/custom/ArticleCustomRepository.java
@@ -1,16 +1,14 @@
 package com.toy.blog.domain.repository.custom;
 
-
 import com.toy.blog.domain.entity.Article;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ArticleCustomRepository {
 
-    List<Article> findAllArticleBySearch(String title, String content, String nickname, Integer page, Integer size);
-
-    List<Article> findAllFollowedArticleBySearch(String title, String content, String nickname, Integer page, Integer size);
+    List<Article> getArticleList(Integer page, Integer size);
 
     Optional<Article> findArticleWithUserBy(Long id);
 
@@ -19,4 +17,6 @@ public interface ArticleCustomRepository {
     void editArticle(Long id, Long userId, String title, String content);
 
     void deleteArticle(Long id);
+
+    List<Article> getFollowArticleList(List<Long> friendIdList, Pageable pageable);
 }
--- domain/src/main/java/com/toy/blog/domain/repository/custom/impl/ArticleCustomRepositoryImpl.java
+++ domain/src/main/java/com/toy/blog/domain/repository/custom/impl/ArticleCustomRepositoryImpl.java
@@ -1,11 +1,13 @@
 package com.toy.blog.domain.repository.custom.impl;
 
 import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.querydsl.jpa.impl.JPAUpdateClause;
 import com.toy.blog.domain.common.Status;
 import com.toy.blog.domain.entity.Article;
 import com.toy.blog.domain.repository.custom.ArticleCustomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -20,31 +22,20 @@ public class ArticleCustomRepositoryImpl implements ArticleCustomRepository {
 
     private final EntityManager entityManager;
 
+//    private JPAQueryFactory queryFactory;
+//
+//    public ArticleCustomRepositoryImpl(EntityManager em) {
+//        this.queryFactory = new JPAQueryFactory(em);
+//    }
+
     /**
      * 게시글 목록 조회
      */
-    public List<Article> findAllArticleBySearch(String title, String content, String nickname, Integer page, Integer size) {
-        return new JPAQuery<Article>(entityManager)
-                .from(article)
-                .join(article.user, user)
-                .where(article.title.like(title)
-                        .or(article.content.like(content)
-                                .or(article.user.nickname.like(nickname))))
-                .limit(size).offset(page)
-                .fetch();
-    }
-
-    /**
-     * 팔로우한 친구의 게시글 목록 조회
-     */
     @Override
-    public List<Article> findAllFollowedArticleBySearch(String title, String content, String nickname, Integer page, Integer size) {
+    public List<Article> getArticleList(Integer page, Integer size) {
         return new JPAQuery<Article>(entityManager)
                 .from(article)
-                .join(article.user, user)
-                .where(article.title.like(title)
-                        .or(article.content.like(content)
-                                .or(article.user.nickname.like(nickname))))
+                .where(article.status.eq(Status.Article.ACTIVE))
                 .limit(size).offset(page)
                 .fetch();
     }
@@ -52,6 +43,7 @@ public class ArticleCustomRepositoryImpl implements ArticleCustomRepository {
     /**
      * id로 게시글 조회
      */
+    @Override
     public Optional<Article> findArticleWithUserBy(Long id) {
         return Optional.ofNullable(new JPAQuery<Article>(entityManager)
                 .from(article)
@@ -75,6 +67,7 @@ public class ArticleCustomRepositoryImpl implements ArticleCustomRepository {
     /**
      * 게시글 수정
      */
+    @Override
     public void editArticle(Long id, Long userId, String title, String content) {
         new JPAUpdateClause(entityManager, article)
                 .set(article.title, article.title.append(title))
@@ -89,6 +82,7 @@ public class ArticleCustomRepositoryImpl implements ArticleCustomRepository {
     /**
      * 게시글 삭제
      */
+    @Override
     public void deleteArticle(Long id) {
         new JPAUpdateClause(entityManager, article)
                 .set(article.status, Status.Article.INACTIVE)
@@ -96,5 +90,27 @@ public class ArticleCustomRepositoryImpl implements ArticleCustomRepository {
                 .execute();
     }
 
+    /**
+     * 게시글 좋아요 증가
+     * To do 구현
+     */
+
+    /**
+     * 팔로우한 친구의 게시글 목록 조회
+     * To do 구현
+     */
+    @Override
+    public List<Article> getFollowArticleList(List<Long> friendIdList, Pageable pageable) {
+        return null;
+//        return queryFactory
+//                .select(article)
+//                .from(article)
+//                .join(article.user, user).fetchJoin()
+//                .where(article.user.id.in(friendIdList))
+//                .offset(pageable.getOffset())
+//                .limit(pageable.getPageSize())
+//                .orderBy(article.createdAt.desc())
+//                .fetch();
+    }
 
 }
