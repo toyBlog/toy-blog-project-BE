8d05b44ab791d906e3281a72a9f0ad0212ef88c1
clapbin <clapbin0320@gmail.com>
2023-04-23 01:01:41 +0900

    feat: ArticleController API 추가
    - 게시글 목록 조회 API 추가
    - 게시글 상세 보기 API 추가
    - 글 작성 API 추가
    - 글 수정 API 추가
    - 글 삭제 API 추가

--- api/src/main/java/com/toy/blog/api/controller/ArticleController.java
+++ api/src/main/java/com/toy/blog/api/controller/ArticleController.java
@@ -1,15 +1,13 @@
 package com.toy.blog.api.controller;
 
+import com.toy.blog.api.model.request.ArticleRequest;
 import com.toy.blog.api.model.response.ArticleResponse;
 import com.toy.blog.api.model.response.Response;
 import com.toy.blog.api.service.ArticleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,9 +19,65 @@ public class ArticleController {
     private final ArticleService articleService;
 
     /**
-     * [API. ] : follower들의 게시물 조회
+     * [API. ] : 게시글 목록 조회
+     */
+    @GetMapping("/articles")
+    public Response<List<ArticleResponse.Search>> getArticles(ArticleRequest.Search request) {
+        return Response.<List<ArticleResponse.Search>>builder()
+                .code(HttpStatus.OK.value())
+                .data(articleService.getArticles(request))
+                .build();
+    }
+
+    /**
+     * [API. ] : 게시글 상세 보기
+     */
+    @GetMapping("/articles/{id}")
+    public Response<ArticleResponse.Detail> getArticle(@PathVariable Long id) {
+        return Response.<ArticleResponse.Detail>builder()
+                .code(HttpStatus.OK.value())
+                .data(articleService.getArticle(id))
+                .build();
+    }
+
+    /**
+     * [API. ] : 글 작성
+     */
+    @PostMapping("/articles")
+    public void insertArticle(@RequestBody ArticleRequest.Register request) {
+        articleService.insertArticle(request);
+    }
+
+    /**
+     * [API. ] : 글 수정
+     */
+    @PatchMapping("/articles/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public Response<Void> editArticle(@PathVariable Long id, ArticleRequest.Register request) {
+        articleService.editArticle(id, request);
+        return Response.<Void>builder().build();
+    }
+
+    /**
+     * [API. ] : 글 삭제
+     */
+    @DeleteMapping("/articles/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public Response<Void> deleteArticle(@PathVariable Long id) {
+        articleService.deleteArticle(id);
+        return Response.<Void>builder().build();
+    }
+
+    /**
+     * [API. ] : 좋아요
+     * To do 구현
+     */
+
+    /**
+     * [API. ] : 팔로우한 친구의 게시글 목록 조회
+     * To do 구현
      * */
-//    @GetMapping("/follow/list/{userId}")
+//    @GetMapping("/articles/follow/{userId}")
 //    public Response<List<ArticleResponse.Search>> getFollowArticleList(@PathVariable Long userId, Pageable pageable){
 //
 //        return Response.<List<ArticleResponse.Search>>builder()
@@ -31,6 +85,5 @@ public class ArticleController {
 //                .code(HttpStatus.OK.value())
 //                .build();
 //    }
-
 
 }
